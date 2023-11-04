a03f32d44e991197b901df3e1e52ae160fcb5c36
ikalwani <ikalwani@umich.edu>
2023-11-04 16:24:17 -0400

    hedhjfc

--- Desktop/EECS485/p4-mapreduce/mapreduce/manager/__main__.py
+++ Desktop/EECS485/p4-mapreduce/mapreduce/manager/__main__.py
@@ -11,16 +11,12 @@ import socket
 import sys 
 from queue import Queue
 
-
 # Configure logging
 LOGGER = logging.getLogger(__name__)
 
 
 class Manager:
     """Represent a MapReduce framework Manager node."""
-    
-# TCP AND UDP socket for manager and worker to communicate 
-# Basically - i think we need 3 threads 
 
     def __init__(self, host, port):
         """Construct a Manager instance and start listening for messages."""
@@ -30,75 +26,67 @@ class Manager:
         self.port = port 
         # store connected workers 
         self.workers = []
+        self.shutdown = False
         # handle incoming messages 
+        # MAYBE USE LIST []
         self.message_queue = Queue()
-        
-        # first create socket to habdle incoming messages 
-        self.setup_socket()
-        
-        # start thread to handle incoming worker connections - TCP
-        accept_thread = threading.Thread(target=self.accept_workers)
+        accept_thread = threading.Thread(target=self.socket)
         accept_thread.start()
-        
+
         LOGGER.info(
             "Starting manager host=%s port=%s pwd=%s",
             host, port, os.getcwd(),
         )
-
-    def setup_socket(self):
+    
+    def socket(self):
         """create socket to listen for incoming connections"""
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        LOGGER.info("Starting manager host=%s port=%s pwd=%s", self.host, self.port, os.getcwd())
-        self.server_socket.settimeout(1)
-    
-    def accept_workers(self):
-        """accept incoming connections from workers"""
-        while True:
-            worker_socket, addr = self.server_socket.accept()
-            LOGGER.info(f"Accepted connection from {addr}")
-            self.workers.append(worker_socket)
-            self.worker_handler(worker_socket)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        self.sock.listen()
+        self.sock.settimeout(1)
+        
+        while True: 
+            try:
+                workersocket, address = self.sock.accept()
+            except socket.timeout:
+                continue
+            print("Connection from", address[0])
             
-    def worker_handler(self, worker_socket):
-        """handle communication with a connected worker"""
-        while True:
-            # receive and process messages from the worker
-            data = worker_socket.recv(4096).decode()
-            if not data:
-                # worker disconnected
-                LOGGER.info("Worker disconnected")
-                self.workers.remove(worker_socket)
-                worker_socket.close()
-                break
+            with workersocket:
+                temp = []
+                while True:
+                    try:
+                        data = workersocket.recv(4096)
+                    except socket.timeout:
+                        continue
+                    if not data:
+                        break
+                    temp.append(data)
+            message_bytes = b''.join(temp)
+            message_str = message_bytes.decode("utf-8")
 
-            # process received data
-            self.message_queue.put(data)
-    
+            try:
+                message_dict = json.loads(message_str)
+                if message_dict == {"message_type": "shutdown"}:
+                    self.shutdown = True
+                    self.message_queue.clear()
+            except json.JSONDecodeError:
+                continue
+            self.message_queue.put(message_dict)
+        
     def shutdown(self):
         """send a shutdown message to all connected workers"""
-        shutdown_message = {"message_type": "shutdown"}
-        for worker_socket in self.workers:
+        
+        for workersocket in self.workers:
             try:
-                worker_socket.send(json.dumps(shutdown_message).encode())
+                workersocket.send(json.dumps({"message_type": "shutdown"}).encode())
             except Exception as e:
                 LOGGER.error("Failed to send shutdown message to worker: %s", str(e))
             finally:
-                # close the worker socket
-                worker_socket.close()
-        sys.exit(0)
-    
-    def start(self):
-        try:
-            while True:
-                command = input("Enter 'shutdown' to initiate a server shutdown: ")
-                if command.strip() == 'shutdown':
-                    self.shutdown()
-        except KeyboardInterrupt:
-            LOGGER.info("Manager interrupted.")
-            self.shutdown()
+                workersocket.close()
+        # LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
 
 
 @click.command()
